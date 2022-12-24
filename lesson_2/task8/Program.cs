40d66d4a702b8f5be059eaf2675da5fd8b6e9e17
Lavrenkov <andrew.lavr@gmail.com>
2022-12-24 22:18:33 +0600

    add lesson 2 -> task8

--- lesson_2/task8/Program.cs
+++ lesson_2/task8/Program.cs
@@ -1,9 +1,12 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-//
 //456 -> 5
 //782 -> 8
 //918 -> 1
-Console.WriteLine("Задача 10");
 
+Console.WriteLine("Задача 10: ");
 
+Console.Write("Введите число : ");
+string a1 = Console.ReadLine();
 
+if (a1.Length<2) Console.WriteLine($"{a1} -> нет данных");
+else Console.WriteLine($"{a1} -> {a1[1]}");
