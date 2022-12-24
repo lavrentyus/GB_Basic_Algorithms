2f77e9a1c17f0005f39c1b7f117b2ccdf50529f1
Lavrenkov <andrew.lavr@gmail.com>
2022-12-24 22:27:37 +0600

    add lesson 2 -> task13

---
+++ lesson_2/task13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.(можно использовать индексацию строк)
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Задача 13: ");
+
+Console.Write("Введите число : ");
+getSimbol(Console.ReadLine(), 2);
+
+
+
+void getSimbol(string s, int simbolNamber)
+{
+    if (s.Length<simbolNamber+1) Console.WriteLine($"{s} -> {simbolNamber+1} цифры нет");
+    else Console.WriteLine($"{s} -> {s[simbolNamber]}");
+}
+
+
