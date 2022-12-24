d240bdde1e9ad559aa4e1f8209c3fea3ebe9ec2d
Lavrenkov <andrew.lavr@gmail.com>
2022-12-25 00:54:14 +0600

    add lesson 3 -> task23

---
+++ lesson_3/task23/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Задача 23: ");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+double[] result = new double[number];
+
+for (int i = 0; i < number; i++)
+{
+    result[i] = Math.Pow(i+1,3);
+}
+
+Console.WriteLine($"{number} -> [{string.Join(", ", result)}]"); 
