beaa4cf811b6ff92fa7e4b21f749992cd8684e07
Lavrenkov <andrew.lavr@gmail.com>
2022-12-24 22:37:16 +0600

    add lesson 2 -> task15

--- lesson_2/task13/Program.cs
+++ lesson_2/task13/Program.cs
@@ -6,11 +6,11 @@
 Console.WriteLine("Задача 13: ");
 
 Console.Write("Введите число : ");
-getSimbol(Console.ReadLine(), 2);
+printSimbol(Console.ReadLine(), 2);
 
 
 
-void getSimbol(string s, int simbolNamber)
+void printSimbol(string s, int simbolNamber)
 {
     if (s.Length<simbolNamber+1) Console.WriteLine($"{s} -> {simbolNamber+1} цифры нет");
     else Console.WriteLine($"{s} -> {s[simbolNamber]}");
---
+++ lesson_2/task15/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine("Задача 15: ");
+
+
+Console.Write("ведите номер дня недели (1-7): ");
+int dayNamber = Convert.ToInt32(Console.ReadLine());
+
+while (dayNamber<1 || dayNamber>7)
+{
+    Console.Write("Ошибка ввода: Введите номер дня недели (1-7): ");
+    dayNamber = Convert.ToInt32(Console.ReadLine());
+}
+
+if (dayNamber%6==0 || dayNamber%7==0) Console.WriteLine($"{dayNamber} -> выходной");
+else Console.WriteLine($"{dayNamber} -> рабочий день");
